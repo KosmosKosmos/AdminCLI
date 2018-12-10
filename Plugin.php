82b9a8f533373a59c8363eeacbecf4771c772be0
Vladimir Bubar <vladimir.bubar@kosmoskosmos.de>
2018-12-10 17:03:00 +0100

    Set Locale Command

--- Plugin.php
+++ Plugin.php
@@ -35,6 +35,7 @@ class Plugin extends PluginBase
         $this->registerConsoleCommand('admincli.setstyles', 'KosmosKosmos\AdminCLI\Console\SetStyles');
         $this->registerConsoleCommand('admincli.frontendusergroup', 'KosmosKosmos\AdminCLI\Console\FrontendUserGroup');
         $this->registerConsoleCommand('admincli.backenduserrole', 'KosmosKosmos\AdminCLI\Console\BackendUserRole');
+        $this->registerConsoleCommand('admincli.setlocale', 'KosmosKosmos\AdminCLI\Console\SetLocale');
     }
 
     /**
---
+++ console/SetLocale.php
@@ -0,0 +1,63 @@
+<?php namespace KosmosKosmos\AdminCLI\Console;
+
+use Illuminate\Console\Command;
+use RainLab\Translate\Models\Locale;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class SetLocale extends Command
+{
+    /**
+     * @var string The console command name.
+     */
+    protected $name = 'admincli:setlocale';
+
+    /**
+     * @var string The console command description.
+     */
+    protected $description = 'Adds locale to Rainlab Translate Locales table';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->option('clearall')) {
+            Locale::truncate();
+        }
+        $locale = Locale::where('code', '=', $this->argument('code'))->first();
+        if (!$locale) {
+            $locale = new Locale();
+        }
+        $locale->code = $this->argument('code');
+        $locale->name = ($this->argument('name') ? $this->argument('name') : $this->argument('code'));
+        $locale->is_default = ($this->option('default') ? true : false);
+        $locale->is_enabled = true;
+        $locale->save();
+    }
+
+    /**
+     * Get the console command arguments.
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['code', InputArgument::REQUIRED, 'Code'],
+            ['name', InputArgument::OPTIONAL, 'Name'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['default', 'd', InputOption::VALUE_NONE, 'Make locale default'],
+            ['clearall', 'c', InputOption::VALUE_NONE, 'Clear all existing locales before adding one']
+        ];
+    }
+}
